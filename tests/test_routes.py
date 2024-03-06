8e12407b635f8d547fae234a53438b40a975454b
msuryarao2011 <159034887+msuryarao2011@users.noreply.github.com>
2024-03-06 22:25:59 +0530

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -174,6 +174,13 @@ class TestProductRoutes(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         data = response.get_json()
         self.assertEqual(data["name"], test_product.name)
+
+    def test_get_product_not_found(self):
+        """It should not Get a Product thats not found"""
+        response = self.client.get(f"{BASE_URL}/0")
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        data = response.get_json()
+        self.assertIn("was not found", data["message"])
     ######################################################################
     # Utility functions
     ######################################################################
